9f97747800f7056b1ebcc73ff1c6dce8380def43
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-09 19:21:04 -0500

    set up set state

--- src/components/fancy-buttons/AngryButton.js
+++ src/components/fancy-buttons/AngryButton.js
@@ -3,9 +3,16 @@
   function AngryButton(){
 
     const [anger, setAnger] = useState(0);
+    const handleClick = () => {
+      if (anger < 1 ) {
+        setAnger(anger + 0.1);
+      } else {
+        setAnger(0);
+      }
+    }
 
     return (
-      <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}}>
+      <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}} onClick={handleClick}>
         {anger < 1 && <span>Don't click me too much! </span>}
         {anger > 1 && <span>Rawr!</span>}
       </button>
