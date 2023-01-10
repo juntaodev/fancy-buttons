6341472302f760b8aa2d87284a8a4afdf33095ce
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-09 19:00:14 -0500

    set up app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import AngryButton from './components/fancy-buttons/AngryButton';
+import CounterButton from './components/fancy-buttons/CounterButton';
+import LightSwitchButton from './components/fancy-buttons/LightSwitchButton';
+import TextRepeaterButton from './components/fancy-buttons/TextRepeaterButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
+    </div>
+  );
+}
+
+export default App;
